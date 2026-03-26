49b957d9c3c64ed10b2e258d54f95af725f24932
Danilo Costa <823119129@ulife.com.br>
2026-03-25 22:25:19 -0300

    feat: implement database connection with mysql using jdbc

--- src/main/java/br/com/siam/Main.java
+++ src/main/java/br/com/siam/Main.java
@@ -1,4 +1,20 @@
 package br.com.siam;
 
+import br.com.siam.config.DatabaseConnection;
+
+import java.sql.Connection;
+
 public class Main {
+    public static void main(String[] args) {
+
+        try (Connection conn = DatabaseConnection.getConnection()) {
+            if (conn != null) {
+                System.out.println("✅ Conectado com sucesso ao MySQL!");
+            }
+        } catch (Exception e) {
+            System.out.println("❌ Erro ao conectar:");
+            e.printStackTrace();
+        }
+
+    }
 }
