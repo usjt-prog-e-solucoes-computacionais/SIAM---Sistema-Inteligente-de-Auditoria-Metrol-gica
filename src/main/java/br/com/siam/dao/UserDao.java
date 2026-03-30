2a8fabda871aab76312fc3dd4a327f7984ed58dc
Danilo Costa <823119129@ulife.com.br>
2026-03-30 14:50:31 -0300

    feat: create user dao interface

---
+++ src/main/java/br/com/siam/dao/UserDao.java
@@ -0,0 +1,19 @@
+package br.com.siam.dao;
+
+import br.com.siam.model.User;
+import java.util.List;
+import java.util.Optional;
+
+public interface UserDao {
+    void insert(User user);
+
+    Optional<User> findById(Integer id);
+
+    Optional<User> findByLogin(String login);
+
+    List<User> findAll();
+
+    boolean update(User user);
+
+    boolean deleteById(Integer id);
+}
